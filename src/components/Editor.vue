7221728579531e5154cc5cafb88c4c394c02e760
Lin-Ya <fenghuiwing@outlook.com>
2018-04-06 21:32:27 +0800

    基本样式

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,45 @@
 <template>
   <div id="Editor">
+    <div class="field">
+    </div>
+    <div class="subfield">
+      <span>
+      </span>
+    </div>
   </div>
 </template>
 
 <style lang="less">
-
+#Editor {
+  display: flex;
+  flex-direction: row;
+  .field {
+    width: 80px;
+    height: 100%;
+    background-color: black;
+    svg {
+      fill: #fff;
+    }
+  }
+  .subfield {
+    flex: 1;
+    overflow: hidden;
+  }
+}
 </style>
 
 <script>
+import store from '../Vuex/store.js';
 export default {
-  name:'Editor',
-}
+  store,
+  data(){
+    return {
+    }
+  },
+  computed: {
+    config(){
+      return this.$store.state.resume.config
+    }
+  }
+};
 </script>
