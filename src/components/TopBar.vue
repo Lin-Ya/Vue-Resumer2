261bd5992204544f3341235fad3930c74ef57161
Lin-Ya <fenghuiwing@outlook.com>
2018-04-06 15:13:16 +0800

    topbar样式

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,11 +1,35 @@
 <template>
   <div id="Topbar">
-      <span>haha</span>
+      <div class="title">
+        <span>Resume Editor</span>
+      </div>
+      <div class="action">
+        <el-button type="success">注册</el-button>
+        <el-button>登录</el-button>
+        <el-button>预览</el-button>
+      </div>
   </div>
 </template>
 
 <style lang="less">
-
+#Topbar {
+  background-color: #fff;
+  padding: 0 16px;
+  display: flex;
+  justify-content: space-between;
+  // align-self: center;
+  // text-align: center;
+  .title {
+    padding: 22px 0;
+    span {
+      font-size: 40px;
+    }
+  }
+  .action {
+    display: flex;
+    align-self: center;
+  }
+}
 </style>
 
 <script>
