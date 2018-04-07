262ad26a0977cef75fc4e3179713574607535d6a
Lin-Ya <fenghuiwing@outlook.com>
2018-04-07 13:24:17 +0800

    v-for循环实现nav-tab切换

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,121 +2,100 @@
   <div id="Editor">
     <nav class="field">
       <ul>
-        <li v-bind:class="{active: navselected === 'profile'}" v-on:click="navselected = 'profile'">
+        <li v-for="item in resume.config" v-bind:key="item.field" v-bind:class="{active: navselected === item.field}" v-on:click="navselected = item.field">
           <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-profile"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: navselected === 'education'}" v-on:click="navselected = 'education'">
-          <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-education"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: navselected === 'workHistory'}" v-on:click="navselected = 'workHistory'">
-          <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-workHistory"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: navselected === 'projects'}" v-on:click="navselected = 'projects'">
-          <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-projects"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: navselected === 'awards'}" v-on:click="navselected = 'awards'">
-          <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-awards"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: navselected === 'contacts'}" v-on:click="navselected = 'contacts'">
-          <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-contacts"></use>
-          </svg>
-        </li>
-        <li v-bind:class="{active: navselected === 'others'}" v-on:click="navselected = 'others'">
-          <svg class="icon" aria-hidden="true">
-            <use xlink:href="#icon-others"></use>
-          </svg>
+              <use v-bind:xlink:href="`#icon-${item.field}`"></use>
+            </svg>
         </li>
       </ul>
     </nav>
     <div class="subfield">
       <nav>
-        <div v-bind:class="{active: navselected === 'profile'}">profile</div>
-        <div v-bind:class="{active: navselected === 'education'}">education</div>
-        <div v-bind:class="{active: navselected === 'workHistory'}">workHistory</div>
-        <div v-bind:class="{active: navselected === 'projects'}">projects</div>
-        <div v-bind:class="{active: navselected === 'awards'}">awards</div>
-        <div v-bind:class="{active: navselected === 'contacts'}">contacts</div>
-        <div v-bind:class="{active: navselected === 'others'}">others</div>
+        <div v-for="item in resume.config" v-bind:key="item.field" v-bind:class="{active: navselected === item.field}">
+          {{item.field}}
+        </div>
       </nav>
     </div>
   </div>
 </template>
 
 <style lang="less">
-#Editor {
-  display: flex;
-  flex-direction: row;
-  .field {
-    width: 80px;
-    height: 100%;
-    background-color: black;
-    ul {
-      li {
-        cursor: pointer;
-        height: 80px;
-        fill: #fff;
-        svg {
-          width: 48px;
-          height: 48px;
-          padding: 16px;
-          fill: inherit;
+  #Editor {
+    display: flex;
+    flex-direction: row;
+    .field {
+      width: 80px;
+      height: 100%;
+      background-color: black;
+      ul {
+        li {
+          cursor: pointer;
+          height: 80px;
+          fill: #fff;
+          svg {
+            width: 48px;
+            height: 48px;
+            padding: 16px;
+            fill: inherit;
+          }
+          &.active {
+            background-color: #fff;
+            fill: #000;
+          }
         }
+      }
+    }
+    .subfield {
+      flex: 1;
+      overflow: hidden;
+      div {
+        display: none;
         &.active {
-          background-color: #fff;
-          fill: #000;
+          display: block;
         }
       }
     }
   }
-  .subfield {
-    flex: 1;
-    overflow: hidden;
-    div {
-      display: none;
-      &.active {
-        display: block;
-      }
-    }
-  }
-}
 </style>
 
 <script>
-export default {
-  data() {
-    return {
-      navselected: "profile",
-      resume: {
-        config: [
-          { field: "profile" },
-          { field: "education" },
-          { field: "workHistory" },
-          { field: "projects" },
-          { field: "awards" },
-          { field: "contacts" },
-          { field: "others" }
-        ],
-        profile:[],
-        education:[],
-        workHistory:[],
-        projects:[],
-        awards:[],
-        contacts:[],
-        others:[],
-      }
-    };
-  },
-  computed: {}
-};
+  export default {
+    data() {
+      return {
+        navselected: "profile",
+        resume: {
+          config: [{
+              field: "profile"
+            },
+            {
+              field: "education"
+            },
+            {
+              field: "workHistory"
+            },
+            {
+              field: "projects"
+            },
+            {
+              field: "awards"
+            },
+            {
+              field: "contacts"
+            },
+            {
+              field: "others"
+            }
+          ],
+          profile: [],
+          education: [],
+          workHistory: [],
+          projects: [],
+          awards: [],
+          contacts: [],
+          others: []
+        }
+      };
+    },
+    computed: {}
+  };
 </script>
