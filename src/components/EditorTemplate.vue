5b1ec7085b850ca6c01f918142b35c03424843d0
Lin-Ya <fenghuiwing@outlook.com>
2018-04-08 23:13:06 +0800

    修复删除按钮位置重叠的bug

--- src/components/EditorTemplate.vue
+++ src/components/EditorTemplate.vue
@@ -17,79 +17,83 @@
 
 <style lang="less">
 .container {
-	position: relative;
-	padding: 0 16px;
-	height: 100%;
-	.header {
-		h1 {
-			line-height: 40px;
-		}
-		button {
-			position: absolute;
-			right: 16px;
-			top: 0px;
-		}
-	}
-	.content {
-		position: relative;
+  position: relative;
+  padding: 0 16px;
+  height: 100%;
+  .header {
+    h1 {
+      line-height: 40px;
+    }
+    button {
+      position: absolute;
+      right: 16px;
+      top: 0px;
+    }
+  }
+  .content {
 		.dataItems {
-			border-bottom: 3px solid #ccc;
-			margin-bottom: 8px;
-		}
-		.deleteButton {
-			font-size: 22px;
-			cursor: pointer;
-			position: absolute;
-			top: 12px;
-			right: 0;
-			color: #808080;
-			&:hover {
-				color: #a00000;
-			}
-		}
-		label {
-			font-size: 16px;
-		}
-	}
+			position: relative;
+      border-bottom: 3px solid #ccc;
+      margin-bottom: 8px;
+    }
+    .deleteButton {
+      font-size: 22px;
+      cursor: pointer;
+      position: absolute;
+      top: 12px;
+      right: 0;
+      color: #808080;
+      &:hover {
+        color: #a00000;
+      }
+    }
+    label {
+      font-size: 16px;
+    }
+  }
 }
 </style>
 
 <script>
 import store from "../Vuex/store.js";
 export default {
-	store,
-  data(){
-		return {
-			state: this.$store.state
-		}
-	},
+  store,
+  data() {
+    return {
+      state: this.$store.state
+    };
+  },
   //给每一个模板传入的field是一大类的数据，包括其简历条目类别以及内容。
-	props: ["field"],
-	methods: {
-		updateForm(field,dataindex,key,$event){
-			//触发updateForm方法，用于更新input内容到store里面。
-			this.$store.commit('updateForm',{field,dataindex,key,value:$event})
-		},
-		add(){
-			let object = {};
-			object.field = this.field.field;
-			object.data = {}
-			for(let key in this.field.label){
-				let model = {}
-				model = this.field.label[key];
-				object.data[model.key] = ''
-			}
-			this.$store.commit('addFieldData',object)
-		},
-		//删除
-		remove(field,dataindex){
-			let object = {};
-			object.field = this.field.field;
-			object.index = dataindex;
-			this.$store.commit('removeFieldData',object)
-		}
-	},
-	computed: {
-	}
+  props: ["field"],
+  methods: {
+    updateForm(field, dataindex, key, $event) {
+      //触发updateForm方法，用于更新input内容到store里面。
+      this.$store.commit("updateForm", {
+        field,
+        dataindex,
+        key,
+        value: $event
+      });
+    },
+    add() {
+      let object = {};
+      object.field = this.field.field;
+      object.data = {};
+      for (let key in this.field.label) {
+        let model = {};
+        model = this.field.label[key];
+        object.data[model.key] = "";
+      }
+      this.$store.commit("addFieldData", object);
+    },
+    //删除
+    remove(field, dataindex) {
+      let object = {};
+      object.field = this.field.field;
+      object.index = dataindex;
+      this.$store.commit("removeFieldData", object);
+    }
+  },
+  computed: {}
 };
 </script>
