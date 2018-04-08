3793c4d8ddeb682f6f489324e6e4f202aa913542
Lin-Ya <fenghuiwing@outlook.com>
2018-04-08 11:58:58 +0800

    editor引入vuex的进行单向绑定

--- src/Vuex/store.js
+++ src/Vuex/store.js
@@ -4,44 +4,48 @@ import Vuex from 'vuex';
 Vue.use(Vuex)
 
 const state = {
-    navselected: "profile",
-    resume: {
-        config: [{
-            field: "profile"
-        },
-        {
-            field: "education"
-        },
-        {
-            field: "workHistory"
-        },
-        {
-            field: "projects"
-        },
-        {
-            field: "awards"
-        },
-        {
-            field: "contacts"
-        },
-        {
-            field: "others"
-        }
-        ],
-        profile: [],
-        education: [],
-        workHistory: [],
-        projects: [],
-        awards: [],
-        contacts: [],
-        others: []
-    }
+  navselected: "profile",
+  resume: {
+    config: [{
+        field: "profile"
+      },
+      {
+        field: "education"
+      },
+      {
+        field: "workHistory"
+      },
+      {
+        field: "projects"
+      },
+      {
+        field: "awards"
+      },
+      {
+        field: "contacts"
+      },
+      {
+        field: "others"
+      }
+    ],
+    profile: [1],
+    education: [2],
+    workHistory: [3],
+    projects: [4],
+    awards: [5],
+    contacts: [6],
+    others: [7]
+  }
 }
 
 const mutations = {
-
+	switchTab(state,target){
+		console.log('set')
+		this.state.navselected = target;
+	}
 }
 
 export default new Vuex.Store({
-    state, mutations
+  state,
+  mutations
 })
--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,10 +2,10 @@
   <div id="Editor">
     <nav class="field">
       <ul>
-        <li v-for="item in resume.config" v-bind:key="item.field" v-bind:class="{active: navselected === item.field}" v-on:click="navselected = item.field">
+        <li v-for="item in resume.config" v-bind:key="item.field" v-bind:class="{active: navselected === item.field}" v-on:click="switchNavTab(item.field)">
           <svg class="icon" aria-hidden="true">
-              <use v-bind:xlink:href="`#icon-${item.field}`"></use>
-            </svg>
+            <use v-bind:xlink:href="`#icon-${item.field}`"></use>
+          </svg>
         </li>
       </ul>
     </nav>
@@ -20,86 +20,70 @@
 </template>
 
 <style lang="less">
-  #Editor {
-    display: flex;
-    flex-direction: row;
-    .field {
-      width: 80px;
-      height: 100%;
-      background-color: black;
-      ul {
-        li {
-          cursor: pointer;
-          height: 80px;
-          fill: #fff;
-          svg {
-            width: 48px;
-            height: 48px;
-            padding: 16px;
-            fill: inherit;
-          }
-          &.active {
-            background-color: #fff;
-            fill: #000;
-          }
+#Editor {
+  display: flex;
+  flex-direction: row;
+  .field {
+    width: 80px;
+    height: 100%;
+    background-color: black;
+    ul {
+      li {
+        cursor: pointer;
+        height: 80px;
+        fill: #fff;
+        svg {
+          width: 48px;
+          height: 48px;
+          padding: 16px;
+          fill: inherit;
         }
-      }
-    }
-    .subfield {
-      flex: 1;
-      overflow: hidden;
-      .subfieldItems {
-        display: none;
         &.active {
-          display: block;
+          background-color: #fff;
+          fill: #000;
         }
       }
     }
   }
+  .subfield {
+    flex: 1;
+    overflow: hidden;
+    .subfieldItems {
+      display: none;
+      &.active {
+        display: block;
+      }
+    }
+  }
+}
 </style>
 
 <script>
-import EditorTemplate from './EditorTemplate'
-  export default {
-    data() {
-      return {
-        navselected: "profile",
-        resume: {
-          config: [{
-              field: "profile"
-            },
-            {
-              field: "education"
-            },
-            {
-              field: "workHistory"
-            },
-            {
-              field: "projects"
-            },
-            {
-              field: "awards"
-            },
-            {
-              field: "contacts"
-            },
-            {
-              field: "others"
-            }
-          ],
-          profile: [1],
-          education: [2],
-          workHistory: [3],
-          projects: [4],
-          awards: [5],
-          contacts: [6],
-          others: [7]
-        }
-      };
-    },
-    computed: {},
-    components: {
-      EditorTemplate
+import EditorTemplate from "./EditorTemplate";
+import store from "../Vuex/store.js";
+export default {
+  store,
+  data() {
+    return {
+    };
+  },
+  methods: {
+    //切换editor的navtab
+    switchNavTab(field){
+      //切换方法位于store里面的mutations
+      this.$store.commit('switchTab',field)
     }
-  };
+  },
+  computed: {
+    resume(){
+      return this.$store.state.resume;
+    },
+    navselected(){
+      return this.$store.state.navselected;
+    }
+  },
+  components: {
+    EditorTemplate
+  }
+};
 </script>
