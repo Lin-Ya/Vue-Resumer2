f9fdc262da08600507531276413706d4fa6e9579
Lin-Ya <fenghuiwing@outlook.com>
2018-04-11 14:44:27 +0800

    拒绝undefined或者null的出现

--- src/lib/getAVUser.js
+++ src/lib/getAVUser.js
@@ -1,6 +1,16 @@
 import AV from "../lib/leancloud";
 
 export default function (user) {
-    var {id,attributes:{username}} = user||AV.User.current()||{attributes:{}}
-    return {id,username}
+  var {
+    id,
+    attributes: {
+      username
+    }
+  } = user || AV.User.current() || {
+    attributes: {}
+  }
+  return {
+    id: id || '',
+    username: username || ''
+  }
 }
