f2a9eb39a4160262710bb74741d4baae68a7740a
Lin-Ya <fenghuiwing@outlook.com>
2018-05-23 17:20:01 +0800

    添加 side details

--- src/components/side/page-side.vue
+++ src/components/side/page-side.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="side">
-    
+    <SideDetails></SideDetails>
   </div>
 </template>
 
 <script>
-export default {};
+import SideDetails from './side-details.vue'
+export default {
+  name: 'side',
+  components: {
+    SideDetails
+  }
+};
 </script>
 
 <style lang="less" scoped>
 #side {
   border: 1px solid red;
   width: 36%;
-  padding: 40px 48px;
+  padding: 80px 48px;
 }
 </style>
---
+++ src/components/side/side-details.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="details">
+    <div class="warper">
+      <ItemSection :section="section"></ItemSection>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemInfo from "base/item-info.vue";
+import ItemSection from "base/item-section";
+import ItemImagePacker from "base/item-imagePacker.vue";
+export default {
+  name: "",
+  data() {
+    return {
+      section: {
+        title: {
+          style: "lightTitle",
+          content: "Details"
+        },
+        list: [
+          {
+            date: {
+              content: '08 1994'
+            },
+            email: {
+              style: 'light',
+              content: 'fenghuiwing@outlook.com'
+            },
+            tel: {
+              content: '132 0261 7737'
+            }
+          }
+        ]
+      }
+    };
+  },
+  components: {
+    ItemInfo,
+    ItemSection,
+    ItemImagePacker
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
